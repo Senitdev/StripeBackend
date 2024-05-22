781e69dc2875d5151af579f58bddeb2badef3517
Senitdev <pape.toure@senitdev.com>
2024-05-22 11:17:27 +0000

    First commit

---
+++ src/main/java/com/stripe/Demo/paiement/avec/stripe/DemoPaiementAvecStripeApplication.java
@@ -0,0 +1,13 @@
+package com.stripe.Demo.paiement.avec.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaiementAvecStripeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPaiementAvecStripeApplication.class, args);
+	}
+
+}
